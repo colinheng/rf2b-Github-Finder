568773691761860dc1ba9c6b9942bdc9989207f1
Colin Heng <colinheng@gmail.com>
2023-08-13 21:12:09 +0800

    section 10 complete

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   }
 
@@ -36,6 +38,53 @@ export const GithubProvider = ({children}) => {
     })
   }
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
+      })
+
+    if (response.status === 404){
+      window.location = "/notfound"
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+  const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
+      })
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    })
+  }
+
+
   // Clear users from state
   const clearUsers = () => dispatch({type:'CLEAR_USERS'})
 
@@ -45,9 +94,13 @@ export const GithubProvider = ({children}) => {
   return (
     <GithubContext.Provider value={{
       users: state.users,
+      user: state.user,
       loading: state.loading,
+      repos: state.repos,
       searchUsers,
       clearUsers,
+      getUser,
+      getUserRepos,
       }}>
         {children}
     </GithubContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <h1 className="text-6xl">Miaow!</h1>
-      <div>{Math.random() * 100}</div>
+      <div>Cheap RNG trick: {Math.random() * 100}</div>
       <UserSearch />
       <UserResults />
     </>
